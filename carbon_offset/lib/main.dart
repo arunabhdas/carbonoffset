a34158a9b83341c147aaa618a0a02f5b3c275280
Arunabh Das <arunabhdas@gmail.com>
2019-11-24 15:43:00 -0500

    Added colors to tabs

--- carbon_offset/lib/main.dart
+++ carbon_offset/lib/main.dart
@@ -17,14 +17,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: DefaultTabController(
@@ -52,6 +44,23 @@ class MyApp extends StatelessWidget {
 
               ],
             ),
+          ),
+          body: TabBarView(
+            children: [
+              new Card(
+                color: Colors.yellow,
+                child: Center(child: Text('Tab1')),
+              ),
+              new Card(
+                color: Colors.orange,
+                child: Center(child: Text('Tab2')),
+              ),
+              new Card(
+                color: Colors.lightGreen,
+                child: Center(child: Text('Tab3')),
+              )
+            ],
+
           ),
         ),
       )
